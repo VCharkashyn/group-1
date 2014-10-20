1c19398dca5aebd8e9817ea94054c376c7cb6d1f
Valiantsin Charkashy <valiantsin_charkashyn@epam.com>
2014-10-20 11:53:21 +0300

    clean master branch

--- jmp-test/run2.bat
+++
@@ -1,4 +0,0 @@
-set MAVEN_OPTS= -verbose:gc -Xmx320m -XX:NewRatio=4 -Xss1m -XX:+PrintTenuringDistribution -XX:SurvivorRatio=1
-mvn clean install -Ptask1 
-mvn clean install -Ptask1 -Dobj
-mvn clean install -Ptask1 -Dscalar
